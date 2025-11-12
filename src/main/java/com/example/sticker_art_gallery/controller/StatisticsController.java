64297e18c45ea4976b8e5d7e52c3b71b7d140e65
Andrey <e13.nst@gmail.com>
2025-11-12 13:10:09 +0300

    Make statistics endpoint public

--- src/main/java/com/example/sticker_art_gallery/controller/StatisticsController.java
+++ src/main/java/com/example/sticker_art_gallery/controller/StatisticsController.java
@@ -3,18 +3,14 @@ package com.example.sticker_art_gallery.controller;
 import com.example.sticker_art_gallery.dto.StatisticsDto;
 import com.example.sticker_art_gallery.service.statistics.StatisticsService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/statistics")
-@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
 @Tag(name = "Статистика сервиса", description = "Агрегированные показатели для главной страницы")
-@SecurityRequirement(name = "TelegramInitData")
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
